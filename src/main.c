3159790f45c1d75e0524c041c474c177f8cdf237
takka <takka@tfact.net>
2008-06-04 03:03:38 +0000

    git-svn-id: http://svn.sourceforge.jp/svnroot/gpsp-kai/trunk/gpsp-kai-test@389 e750ed6d-7236-0410-a570-cc313d6b6496

--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
  * Copyright (C) 2006 Exophase <exophase@gmail.com>
  * Copyright (C) 2007 takka <takka@tfact.net>
  * Copyright (C) 2007 NJ
- * Copyright (C) 2007 ????? <?????>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
