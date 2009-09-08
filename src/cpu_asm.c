e18726ce66d20a088ac9fe0682a0f228313c799e
takka <takka@tfact.net>
2009-09-08 00:50:06 +0000

    git-svn-id: http://svn.sourceforge.jp/svnroot/gpsp-kai/trunk/gpsp-kai-test@432 e750ed6d-7236-0410-a570-cc313d6b6496

--- src/cpu_asm.c
+++ src/cpu_asm.c
@@ -274,7 +274,7 @@ typedef struct
     }                                                                         \
   }                                                                           \
                                                                               \
-  switch((opcode >> 20) & 0xFF)                                               \
+  switch((opcode >> 20) & 0xFF) /* 0xXXX0 0000 -> 0xXXX */                    \
   {                                                                           \
     case 0x00:                                                                \
       if((opcode & 0x90) == 0x90)                                             \
